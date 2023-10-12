5d7e9e425ab4b0f8e335bd312adb8426ad735ca4
elmar.amanov <elmar.amanov@cybernet.az>
2023-10-12 10:27:11 +0400

    CREATING USER AND USE REDUX DISPATCH WITH PROMISE AND USE THEN CATCH and use redux islOading

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -7,50 +7,79 @@ function UsersList() {
   const dispatch = useDispatch();
   const [isLoadingUsers, setIsLoadingUsers] = useState(false);
   const [loadingUsersError, setLoadingUsersError] = useState(null);
-  const {isLoading, data, error} = useSelector(state=> state.users);
 
-  useEffect(()=>{
-    setIsLoadingUsers(true)
+  const [isCreatingUser, setIsCreatingUser] = useState(false);
+  const [CreatingUserError, setCreatingUserError] = useState(null);
+  const { isLoading, data, error } = useSelector((state) => state.users);
+
+  useEffect(() => {
+    setIsLoadingUsers(true);
     console.log(dispatch(fetchUsers()));
     dispatch(fetchUsers())
       .unwrap()
-      .then(res=> {
-        console.log({res});
+      .then((res) => {
+        console.log({ res });
       })
-      .catch(error => {
-        setLoadingUsersError(error)
+      .catch((error) => {
+        setLoadingUsersError(error);
         setIsLoadingUsers(false);
       })
-      .finally(()=> {
+      .finally(() => {
         setIsLoadingUsers(false);
       });
-  },[dispatch]);
+  }, [dispatch]);
 
   const handleAddUser = () => {
+    setIsCreatingUser(true);
     dispatch(addUserThunk())
+      .unwrap()
+      .catch((err) => setCreatingUserError(err))
+      .finally(() => setIsCreatingUser(false));
   };
 
   // if(isLoading ) return <Skeleton times={6} className='h-10 w-full'/>;
-  if(isLoadingUsers) return <Skeleton times={6} className='h-10 w-full'/>;
+  if (isLoadingUsers) return <Skeleton times={6} className="h-10 w-full" />;
 
   // if(error) return <div> Errors </div>;
-  if(loadingUsersError) return <div> Errors </div>;
+  if (loadingUsersError) return <div> Errors </div>;
 
-  return <>
-  <div style={{display: 'flex', flexDirection:'row', justifyContent:'space-between'}}>
-    <h1>USERS</h1>
-    <div>
-      <button onClick={handleAddUser} style={{background: 'gray'}}> Add user</button>
-    </div>
-  </div>
-  <br/>
-  <hr/>
-  <div>{
-    data && data.map(user=> <ul key={user.id}>
-      <li>Name: {user.name} </li>
-    </ul>)
-  }</div>
-  </>
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <h1>USERS</h1>
+        <div>
+          {isCreatingUser ? (
+            <h1>"Creating AUser " </h1>
+          ) : (
+            <button onClick={handleAddUser} style={{ background: "gray" }}>
+              Add user
+            </button>
+          )}
+          {CreatingUserError && <h1>{JSON.stringify(CreatingUserError)}</h1>}
+        </div>
+      </div>
+      <br />
+      <hr />
+      {isLoading ? (
+        <Skeleton times={6} className="h-10 w-full" />
+      ) : (
+        <div>
+          {data &&
+            data.map((user) => (
+              <ul key={user.id}>
+                <li>Name: {user.name} </li>
+              </ul>
+            ))}
+        </div>
+      )}
+    </>
+  );
 }
 
 export default UsersList;
